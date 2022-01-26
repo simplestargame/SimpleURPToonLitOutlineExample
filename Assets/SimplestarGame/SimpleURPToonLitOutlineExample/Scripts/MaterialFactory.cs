3f8df3130bafb53860f26623c435ad6c5e23f09a
simplestar <>
2022-01-27 08:31:26 +0900

    [add] release package contents

---
+++ Assets/SimplestarGame/SimpleURPToonLitOutlineExample/Scripts/MaterialFactory.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace VRMShaders
+{
+    public delegate Task<Texture> GetTextureAsyncFunc(TextureDescriptor texDesc, IAwaitCaller awaitCaller);
+
+    public class MaterialFactory : IResponsibilityForDestroyObjects
+    {
+        private readonly IReadOnlyDictionary<SubAssetKey, Material> m_externalMap;
+
+        public MaterialFactory(IReadOnlyDictionary<SubAssetKey, Material> externalMaterialMap)
+        {
+            m_externalMap = externalMaterialMap;
+        }
+
+        public struct MaterialLoadInfo
+        {
+            public SubAssetKey Key;
+            public readonly Material Asset;
+            public readonly bool UseExternal;
+
+            public bool IsSubAsset => !UseExternal;
+
+            public MaterialLoadInfo(SubAssetKey key, Material asset, bool useExternal)
+            {
+                Key = key;
+                Asset = asset;
+                UseExternal = useExternal;
+            }
+        }
+
+        List<MaterialLoadInfo> m_materials = new List<MaterialLoadInfo>();
+        public IReadOnlyList<MaterialLoadInfo> Materials => m_materials;
+        void Remove(Material material)
+        {
+            var index = m_materials.FindIndex(x => x.Asset == material);
+            if (index >= 0)
+            {
+                m_materials.RemoveAt(index);
+
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var x in m_materials)
+            {
+                if (!x.UseExternal)
+                {
+                    // 外部の '.asset' からロードしていない
+#if VRM_DEVELOP
+                    // Debug.Log($"Destroy {x.Asset}");
+#endif
+                    UnityObjectDestoyer.DestroyRuntimeOrEditor(x.Asset);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 所有権(Dispose権)を移譲する
+        ///
+        /// 所有権を移動する関数。
+        ///
+        /// * 所有権が移動する。return true => ImporterContext.Dispose の対象から外れる
+        /// * 所有権が移動しない。return false => Importer.Context.Dispose でDestroyされる
+        ///
+        /// </summary>
+        /// <param name="take"></param>
+        public void TransferOwnership(TakeResponsibilityForDestroyObjectFunc take)
+        {
+            foreach (var x in m_materials.ToArray())
+            {
+                if (!x.UseExternal)
+                {
+                    // 外部の '.asset' からロードしていない
+                    take(x.Key, x.Asset);
+                    m_materials.Remove(x);
+                }
+            }
+        }
+
+        public Material GetMaterial(int index)
+        {
+            if (index < 0) return null;
+            if (index >= m_materials.Count) return null;
+            return m_materials[index].Asset;
+        }
+
+        public async Task<Material> LoadAsync(MaterialDescriptor matDesc, GetTextureAsyncFunc getTexture, IAwaitCaller awaitCaller)
+        {
+            if (m_externalMap.TryGetValue(matDesc.SubAssetKey, out Material material))
+            {
+                m_materials.Add(new MaterialLoadInfo(matDesc.SubAssetKey, material, true));
+                return material;
+            }
+
+            if (getTexture == null)
+            {
+                getTexture = (x, y) => Task.FromResult<Texture>(null);
+            }
+
+            var shaderName = matDesc.ShaderName;
+            if (String.IsNullOrEmpty(shaderName))
+            {
+                throw new Exception("no shader name");
+            }
+
+            // Set URP Shader Name
+            shaderName = "SimpleURPToonLitOutlineExample";
+            if (matDesc.RenderQueue.HasValue)
+            {
+                if ((int)UnityEngine.Rendering.RenderQueue.Transparent <= matDesc.RenderQueue.Value)
+                {
+                    shaderName = "Shader Graphs/SimpleURPTransparent";
+                }
+            }
+
+            var shader = Shader.Find(shaderName);
+            if (shader == null)
+            {
+                throw new Exception($"shader: {shaderName} not found");
+            }
+
+            material = new Material(shader);
+            material.name = matDesc.SubAssetKey.Name;
+
+            bool hasShadeMap = false;
+            bool useEmission = false;
+            Texture baseMap = null;
+            foreach (var kv in matDesc.TextureSlots)
+            {
+                var texture = await getTexture(kv.Value, awaitCaller);
+                if (texture != null)
+                {
+                    if (this.TryGetMaterialParamKey(kv.Key, out string key, out float scale))
+                    {
+                        if ("_BaseMap" == key)
+                        {
+                            baseMap = texture;
+                        }
+                        if ("_ShadeMap" == key)
+                        {
+                            hasShadeMap = true;
+                        }
+                        if ("_EmissionMap" == key)
+                        {
+                            useEmission = true;
+                        }
+                        material.SetTexture(key, texture);
+                    }
+                }
+                else
+                {
+                    // 存在するテクスチャはそのまま適用
+                    if (this.TryGetMaterialParamKey(kv.Key, out string key, out float scale))
+                    {
+                        if ("_EmissionMap" == key)
+                        {
+                            useEmission = true;
+                        }
+                        material.SetTexture(key, texture);
+                    }
+                }
+            }
+
+            foreach (var kv in matDesc.Vectors)
+            {
+                if (this.TryGetMaterialParamKey(kv.Key, out string key, out float scale))
+                {
+                    if (matDesc.TextureSlots.ContainsKey(kv.Key))
+                    {
+                        // texture offset & scale
+                        material.SetTextureOffset(key, new Vector2(kv.Value[0] * scale, kv.Value[1] * scale));
+                        material.SetTextureScale(key, new Vector2(kv.Value[2] * scale, kv.Value[3] * scale));
+                    }
+                    else
+                    {
+                        // vector4
+                        var v = new Vector4(kv.Value[0] * scale, kv.Value[1] * scale, kv.Value[2] * scale, kv.Value[3] * scale);
+                        material.SetVector(key, v);
+                    }
+                }
+            }
+
+            bool useOutline = false;
+            foreach (var kv in matDesc.FloatValues)
+            {
+                if (this.TryGetMaterialParamKey(kv.Key, out string key, out float scale))
+                {
+                    if ("_OutlineWidthMode" == key)
+                    {
+                        useOutline = 0.0f != kv.Value;
+                    }
+                    material.SetFloat(key, kv.Value * scale);
+                }
+            }
+
+            if (useEmission)
+            {
+                material.SetFloat("_UseEmission", 1);
+            }
+            if (!useOutline)
+            {
+                material.SetFloat("_OutlineWidth", 0.0001f);
+            }
+
+            if (!hasShadeMap && null != baseMap)
+            {
+                material.SetTexture("_ShadeMap", baseMap);
+            }
+
+            foreach (var kv in matDesc.Colors)
+            {
+                if (this.TryGetMaterialParamKey(kv.Key, out string key, out float scale))
+                {
+                    var color = kv.Value * scale;
+                    material.SetColor(key, color);
+                    if ("_BaseColor" == key && !hasShadeMap)
+                    {
+                        material.SetColor("_ShadeColor", new Color(Mathf.Clamp(color[0] - 0.1f, 0, 1f), Mathf.Clamp(color[1] - 0.2f, 0, 1f), Mathf.Clamp(color[2] - 0.2f, 0, 1f), color[3]));
+                    }
+                }
+            }
+
+            foreach (var action in matDesc.Actions)
+            {
+                action(material);
+            }
+
+            if (matDesc.RenderQueue.HasValue)
+            {
+                material.renderQueue = matDesc.RenderQueue.Value;
+            }
+
+            m_materials.Add(new MaterialLoadInfo(matDesc.SubAssetKey, material, false));
+
+            return material;
+        }
+
+        public static void SetTextureOffsetAndScale(Material material, string propertyName, Vector2 offset, Vector2 scale)
+        {
+            material.SetTextureOffset(propertyName, offset);
+            material.SetTextureScale(propertyName, scale);
+        }
+
+        /// <summary>
+        /// Toon表現用マテリアルにマッチするパラメータキーへの変換
+        /// </summary>
+        /// <param name="keyIn">入力のキー</param>
+        /// <param name="keyOut">変換後のキー</param>
+        /// <param name="scaleValue">値の倍率</param>
+        /// <returns>変換可能</returns>
+        bool TryGetMaterialParamKey(string keyIn, out string keyOut, out float scaleValue)
+        {
+            keyOut = keyIn;
+            scaleValue = 1.0f;
+            bool validKey = true;
+            switch (keyIn)
+            {
+                case "_Color":
+                    keyOut = "_BaseColor";
+                    break;
+                case "_MainTex":
+                    keyOut = "_BaseMap";
+                    break;
+                case "_ShadeColor":
+                    keyOut = "_ShadeColor";
+                    break;
+                case "_ShadeTexture":
+                    keyOut = "_ShadeMap";
+                    break;
+                case "_EmissionColor":
+                    keyOut = "_EmissionColor";
+                    break;
+                case "_EmissionMap":
+                    keyOut = "_EmissionMap";
+                    break;
+                case "_OutlineColor":
+                    keyOut = "_OutlineColor";
+                    break;
+                case "_OutlineWidth":
+                    keyOut = "_OutlineWidth";
+                    scaleValue = 0.005f;
+                    break;
+                case "_OutlineWidthMode":
+                    keyOut = "_OutlineWidthMode";
+                    break;
+                case "_CutOff":
+                    keyOut = "_Cutoff";
+                    break;
+                case "_CullMode":
+                    keyOut = "_Cull";
+                    break;
+                default:
+                    validKey = false;
+                    break;
+            }
+            return validKey;
+        }
+    }
+}
---
+++ Assets/SimplestarGame/SimpleURPToonLitOutlineExample/SimpleURPToonLitOutlineExample/README.md
@@ -0,0 +1,104 @@
+# Unity URP Simplified Toon Lit Shader Example (for you to learn writing custom lit shader in URP)
+
+This repository is NOT the full version shader, the full version shader is still WIP and not yet released.
+This repository is only for tutorial purpose, which only contains a very simple and short shader example.
+
+## What is included in this "simplified version" toon lit shader repository?
+
+This repository contains a very simple toon lit shader example, to help people writing their first custom toon lit shader in URP.
+
+Because this example toon lit shader aims to help people learning shader writing in URP, it is an extremely simplified version of the full version one. This repository only contains ~10% of the full version shader, which only contains the most useful & easy to understand sections, to make sure everyone can understand the shader code easily.
+
+It is actually a "How to write your first custom lit shader in URP" example, instead of a good looking toon lit shader example (lots of toon lit tricks are not included in this example shader, for tutorial reason).
+
+## Why creating this "simplified version" toon lit shader?
+
+Lots of my shader friends are looking for a toon lit example shader in URP (not Shader Graph), I want them to switch to URP with me (instead of still staying in built-in RP), so I decided to provide a simple enough URP toon lit shader example.
+
+## How to try this simplified toon lit example shader in my URP project?
+
+1. Clone all .shader & .hlsl files into your URP project.
+2. Put these files inside the same folder.
+3. Change your character's material's shader to "SimpleURPToonLitExample(With Outline)"
+4. make sure atleast \_BaseMap(albedo) is assigned
+5. setup DONE, you can now test your character with light probe/directional light/point light/spot light
+6. edit the material properties to see how the render result changes
+7. Most important: open these shader files, spend some time reading it, you will understand how to write custom lit shader in URP very quickly
+8. Most important: open "SimpleURPToonLitOutlineExample_LightingEquation.hlsl", edit it, experiment with your own toon lighting equation ideas, which is the key part of toon lit shader!
+
+## What is NOT included in this simplified example shader?
+
+For simplicity reason, I removed most of the features from the Full version shader (deleted 90% of the original shader), else this example shader will be way too complex for reading & learning. The removed features are:
+
+- face sphere proxy normal & direction proxy normal (fixing face ugly normals, very important)
+- smooth outline normal auto baking (fixing ugly outlines, very important)
+- constant outline fix for fov and distance to camera (outline looks correct in any situation, very important)
+- tricks to remove unwanted hair shadow casted on face (to keep face looks clean, very important)
+- tricks to render eye over hair
+- hair "angel ring" reflection
+- sharp rim light
+- PBR specular lighting (GGX)
+- HSV control shadow & outline color
+- 2D mouth renderer
+- stencil local hair shadow on face
+- physics and wind
+
+- almost all the extra texture input options like AO, specular, normal map...
+- LOTS of sliders to control lighting, final color & outline
+- \*\*\*just too much for me to write all removed feature here, the full version shader is a totally different level product
+
+## When will the Full version toon lit shader release?
+
+We don't have ETA now, we are still working on it, here are some videos about the Full version toon lit shader:
+
+- https://youtu.be/uVI_QOioER4
+- https://youtu.be/YtAiCHBvZr0
+- https://youtu.be/QWB060rVjFI
+- https://youtu.be/URVgKT5c3PM
+- https://youtu.be/6Gx0LxByfWk
+- https://youtu.be/IP293mAmBCk
+- https://youtu.be/n_bFQ9GNhcM
+- https://youtu.be/HmuaWpL5Pi8
+- https://youtu.be/tNnqIP4NdV8 (current)
+- https://youtu.be/ZfSZOHTBypc (current)
+- https://youtu.be/cebGl_MaWnI (current)
+- https://youtu.be/Ty4DXLFqqDo (current)
+- https://youtu.be/EgxiWPk-vaE (current)
+- https://youtu.be/gcUCTLF5hwE (current)
+
+## How to get a test character model?
+
+The easiest way to get a character model is by downloading Unity-Chan in the assetstore.
+
+Also, here are some websites that can download models(If the creator allows it)
+
+- https://3d.nicovideo.jp/
+- https://hub.vroid.com/
+
+if you downloaded a .pmx file, use this to convert it to .fbx & prefab directly inside unity
+
+MMD4Mecanim (Beta)
+http://stereoarts.jp/
+
+if you downloaded a .vrm file, use this to convert it to .fbx & prefab directly inside unity
+
+UniVRM
+https://github.com/vrm-c/UniVRM
+
+## Editor environment requirement
+
+- URP 7.2.1 or above
+- Unity 2019.3 or above
+
+---
+
+## More old screenshots from the Full version shader(not yet released):
+
+https://youtu.be/tNnqIP4NdV8
+
+---
+
+Apply our shader to another model (2020-2 early version screen shots)
+https://youtu.be/uVI_QOioER4
+
+https://youtu.be/IP293mAmBCk
